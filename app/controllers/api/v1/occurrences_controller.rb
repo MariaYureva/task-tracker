1c8fd3337194b8e13c24202de33c84bf15df1395
Мария Юрьева <Lakalaka245@ya.ru>
2026-06-12 22:35:36 +0300

    feat: эндпоинт-календарь вхождений (окно, фильтры по статусу и тегу)

---
+++ app/controllers/api/v1/occurrences_controller.rb
@@ -0,0 +1,32 @@
+module Api
+  module V1
+    class OccurrencesController < BaseController
+      MAX_WINDOW_DAYS = 366
+      def index
+        from, to = parse_window!
+
+        tasks = current_user.tasks.active.includes(:tags, :recurrence_dates)
+        tasks = tasks.joins(:task_tags).where(task_tags: { tag_id: params[:tag_id] }).distinct if params[:tag_id].present?
+
+        occurrences = Occurrences::Query.call(tasks, from: from, to: to)
+        occurrences = occurrences.select { |o| o.status == params[:status] } if params[:status].present?
+
+        render json: occurrences.map { |o| OccurrenceSerializer.call(o) }
+      end
+
+      private
+
+      def parse_window!
+        raise ActionController::ParameterMissing, :from if params[:from].blank?
+        raise ActionController::ParameterMissing, :to   if params[:to].blank?
+
+        from = Date.parse(params[:from])
+        to   = Date.parse(params[:to])
+        raise ArgumentError, "`to` must be on or after `from`" if to < from
+        raise ArgumentError, "window must not exceed #{MAX_WINDOW_DAYS} days" if (to - from).to_i > MAX_WINDOW_DAYS
+
+        [from, to]
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
       resources :users, only: %i[index show create]
       resources :tags,  only: %i[index create update destroy]
 
+      resources :occurrences, only: %i[index]
+
       resources :tasks, only: %i[index show create update destroy] do
         resources :tags, only: %i[create destroy], controller: "task_tags"
       end
